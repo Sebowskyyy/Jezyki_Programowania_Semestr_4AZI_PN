44aa15a75b34be218537d8018de880cd55e78d7c
Sebowskyyy <pawel.noga@edu.wsti.pl>
2025-03-19 22:51:38 +0100

    Wszystkie zadanka zrobione:)

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import java.math.BigInteger;
 
 
 public class Main {
-    public static int getIntWithWindow(String windowText) {
+    public static Integer getIntWithWindow(String windowText) {
         int numberToReturn = 0;
         String textGotFromWindow = "";
         boolean isValueParsed = false;
         while (!isValueParsed && textGotFromWindow != null) {
             textGotFromWindow = JOptionPane.showInputDialog(windowText);
+            if (textGotFromWindow == null) {
+                return null;
+            }
             try {
                 numberToReturn = Integer.parseInt(textGotFromWindow);
                 isValueParsed = true;
@@ -18,7 +21,6 @@ public class Main {
                 }
             }
         }
-
         return numberToReturn;
     }
 
@@ -27,11 +29,28 @@ public class Main {
     }
 
     public static void showText(String textToShowInWindow) {
-        JOptionPane.showMessageDialog(null, textToShowInWindow);
+        if (textToShowInWindow != null) {
+            JOptionPane.showMessageDialog(null, textToShowInWindow);
+        }
+    }
+
+    public static void showText(BigInteger textToShowInWindow) {
+        if (textToShowInWindow != null) {
+            JOptionPane.showMessageDialog(null, textToShowInWindow);
+        }
+    }
+
+    public static void showText(Integer textToShowInWindow) {
+        if (textToShowInWindow != null) {
+            JOptionPane.showMessageDialog(null, textToShowInWindow);
+        }
     }
 
     public static String binaryRepresentation() {
-        int numberToConvert = getIntWithWindow("Podaj liczbe a zamienie na bity");
+        Integer numberToConvert = getIntWithWindow("Podaj liczbe a zamienie na system dwójkowy");
+        if (numberToConvert == null) {
+            return null;
+        }
         String binaryRepresentationOfInt = "";
         while (numberToConvert != 1 && numberToConvert != 0) {
             if (numberToConvert % 2 == 1) {
@@ -50,45 +69,88 @@ public class Main {
         return binaryRepresentationOfInt;
     }
 
-    public static String octalRepresentation() {
-        int numberToConvert = getIntWithWindow("Podaj liczbe a zamienie na osemkowy");
-        int octalRepresentHelper = 0;
-        String octalRepresentationOfInt = "";
+    public static String octalRepresentationOfDecimalValue() {
+        Integer decimalValue = getIntWithWindow("Podaj liczbe a zamienie na system ósemkowy");
+        if(decimalValue == null){
+            return null;
+        }
+        String octalRepresentationInString = "";
+        octalRepresentationInString = String.valueOf(decimalValue % 8);
+        decimalValue = decimalValue - decimalValue % 8;
 
 
-        octalRepresentHelper = numberToConvert % 8;
-        octalRepresentationOfInt = octalRepresentationOfInt + octalRepresentHelper;
-        numberToConvert = numberToConvert - octalRepresentHelper;
-        int counter = 0;
-        int firstNumber;
-//        while (numberToConvert > 1) {
-//            counter++;
-//            numberToConvert = numberToConvert / 8;
-//            firstNumber = octalRepresentationOfInt.charAt(1);
-//            if (counter == 8)
-//                counter = counter / 8;
-//                firstNumber = firstNumber + counter;
-//
-//        }
+        while (decimalValue != 0) {
+            decimalValue = decimalValue / 8;
+            if (decimalValue % 8 != 0) {
+                octalRepresentationInString = String.valueOf(decimalValue % 8) + octalRepresentationInString;
+                if (decimalValue - decimalValue % 8 > 0)
+                    decimalValue = decimalValue - (decimalValue % 8); // odjecie
+            } else if (decimalValue != 0 && decimalValue % 8 == 0) {
+                octalRepresentationInString = "0" + octalRepresentationInString;
+            } else if (decimalValue == 1) {
+                octalRepresentationInString = "1" + octalRepresentationInString;
+            }
 
 
-        System.out.println(octalRepresentationOfInt);
+        }
+        return octalRepresentationInString;
+    }
+
+    /// Patrząc na to co wygenerowało AI niżej, chyba lekko przekombinowałem :D, ale przynajmniej to u góry to mój naturalny skrypt
+    /// a nie to co dzisiaj, wszystko sztuczne, hodowane przez AI
+
+/*
+    int liczbaDziesietnaAI = 95648;
+    String sklejkaAI = "";
+
+        while (liczbaDziesietnaAI > 0) {
+        sklejkaAI = (liczbaDziesietnaAI % 8) + sklejkaAI;
+        liczbaDziesietnaAI /= 8;
+    }
+
+        System.out.println(sklejkaAI);
+
+*/
+    public static String hexaDecimalRepresentationOfDecimalValue() {
+        String hexaValueAsString = "";
+        Integer decimalValue = getIntWithWindow("Podaj liczbe a zamienie na system szesnastkowy");
+        if(decimalValue == null){
+            return null;
+        }
+        hexaValueAsString =  String.valueOf(decimalValue % 16) + ",";
+        decimalValue = decimalValue - decimalValue % 16;
 
 
+        while (decimalValue != 0) {
+            decimalValue = decimalValue / 16;
+            if (decimalValue % 16 != 0) {
+                hexaValueAsString = String.valueOf(decimalValue % 16) + "," + hexaValueAsString;
+                if (decimalValue - decimalValue % 16 > 0)
+                    decimalValue = decimalValue - (decimalValue % 16);
+            } else if (decimalValue != 0 && decimalValue % 16 == 0) {
+                hexaValueAsString = "0" + hexaValueAsString;
+            }
 
 
-        return octalRepresentationOfInt;
+        }
+        String[] hexCharacters = {"A", "B", "C", "D", "E", "F"};
+        for (int i = 10; i < 16; i++) {
+            hexaValueAsString = hexaValueAsString.replaceAll(i + ",", hexCharacters[i - 10]);
+        }
+        hexaValueAsString = hexaValueAsString.replaceAll(",", "");
+        return hexaValueAsString;
     }
 
 
     public static BigInteger factorial() {
-        int numberForFactrial;
         BigInteger numberToReturn = new BigInteger("1");
-        numberForFactrial = getIntWithWindow("Podaj liczbe do silni");
+        Integer numberForFactrial = getIntWithWindow("Podaj liczbe do silni");
+        if (numberForFactrial == null) {
+            return null;
+        }
         while (numberForFactrial > 0) {
             numberToReturn = numberToReturn.multiply(BigInteger.valueOf(numberForFactrial));
             numberForFactrial--;
-            System.out.println(numberToReturn);
         }
 
 
@@ -96,7 +158,11 @@ public class Main {
     }
 
 
-    public static int sumOddNumbers(int endingNumber) {
+    public static Integer sumOddNumbers() {
+        Integer endingNumber = getIntWithWindow("Podaj liczbę a ja zsumuje wszelkie wynikające z niej mniejsze nieparzyste liczby");
+        if (endingNumber == null) {
+            return null;
+        }
         int sum = 0;
         while (endingNumber > 0) {
             if (endingNumber % 2 == 1) {
@@ -110,21 +176,17 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //    String textIWantToUpper = getTextWithWindow("Wprowadź litery a zwrócę je z CAPSEM");
-        //    showText(textIWantToUpper.toUpperCase()); //Zad 1.1
-        //    System.out.println(factorial().toString());
-       /* showText(String.valueOf(factorial()));//Zad 1.2
-        int sumOfOdds = sumOddNumbers(Integer.parseInt(getTextWithWindow("Wprowadz liczbe konczaca zakres"))); //zad 1.3
-        showText(String.valueOf(sumOfOdds)); // zad 1.3
-*/
-        binaryRepresentation();
-        //       octalRepresentation();
+        String textIWantToUpper = getTextWithWindow("Wprowadź litery a zwrócę je z CAPSEM");
+        if (textIWantToUpper != null) {// zad 1.1
+            showText(textIWantToUpper.toUpperCase());
+        } //Zad 1.1
+        showText(factorial());//Zad 1.2
+        showText(sumOddNumbers()); // zad 1.3
 
+        showText(binaryRepresentation()); //zad 1.4
+        showText(octalRepresentationOfDecimalValue()); // zad 1.4
+        showText(hexaDecimalRepresentationOfDecimalValue()); // zad 1.4
 
-        String lama = "000111010";
-        String newLama = lama.substring(3);
-
-        System.out.println(newLama);
     }
 
 
